fbcd0bed7c592581245777e32c3ea61b7234dd62
reborg <reborg@reborg.net>
2016-05-05 10:22:29 +0100

    bump versions of clojure and core.async to the latest

--- project.clj
+++ project.clj
@@ -1,16 +1,17 @@
-(defproject throttle "0.1.3"
+(defproject throttle "0.2.0"
   :description "A request throttler library for Clojure."
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [http-kit "2.1.19"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [org.clojure/core.async "0.2.374"]
+
+                 ;; logging stuff
                  [org.clojure/tools.logging "0.2.6"]
                  [ch.qos.logback/logback-classic "1.0.13" :exclusions [org.slf4j/slf4j-api]]
                  [ch.qos.logback/logback-access "1.0.13"]
                  [ch.qos.logback/logback-core "1.0.13"]
                  [org.slf4j/slf4j-api "1.7.5"]]
-  :repl-options {:init (do
-                         (require 'midje.repl)
-                         (midje.repl/autotest :filter :core (complement :slow)))}
+
+  :repl-options {:init (do (require 'midje.repl) (midje.repl/autotest :filter :core (complement :slow)))}
   :aliases {"midje-fast" ["midje" ":filter" "-slow"]}
   :profiles {:dev
              {:dependencies [[midje "1.6.3"]
